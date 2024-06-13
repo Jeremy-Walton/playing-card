15d7005a572e28ce54ce2a8fbd7ff785f14c37c4
Jeremy Walton <jeremy.patrick.walton@gmail.com>
2024-06-12 21:20:20 -0400

    Refactor card and add face down option

--- src/op-playing-card.ts
+++ src/op-playing-card.ts
@@ -1,11 +1,14 @@
+// Lit
 import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
+// Suit images
 import club from './assets/Club.svg'
 import diamond from './assets/Diamond.svg'
 import heart from './assets/Heart.svg'
 import spade from './assets/Spade.svg'
 
+// Face card images
 import jc from './assets/JC.png'
 import jd from './assets/JD.png'
 import jh from './assets/JH.png'
@@ -18,83 +21,94 @@ import qc from './assets/QC.png'
 import qd from './assets/QD.png'
 import qh from './assets/QH.png'
 import qs from './assets/QS.png'
+import back from './assets/back.png'
 
 @customElement('op-playing-card')
 export class OpPlayingCard extends LitElement {
   @property({ type: String })
-  rank = '1'
+  rank = 'A'
 
   @property({ type: String })
   suit = 'S'
 
-  @property({ type: Boolean, reflect: true })
-  red = false
+  @property({ type: Boolean })
+  faceDown = false
 
-  suitIcon() {
-    const suitMap: { [index: string]: string } = {
-      'C': club,
-      'D': diamond,
-      'H': heart,
-      'S': spade,
-    }
-
-    return suitMap[this.suit] ?? spade
+  #isRed() {
+    return ['D', 'H'].includes(this.suit)
   }
 
-  faceCardImage() {
-    const faceMap: { [index: string]: { [index: string]: string } } = {
-      'C': { 'J': jc, 'Q': qc, 'K': kc },
-      'D': { 'J': jd, 'Q': qd, 'K': kd },
-      'H': { 'J': jh, 'Q': qh, 'K': kh },
-      'S': { 'J': js, 'Q': qs, 'K': ks },
-    }
-
-    return faceMap[this.suit][this.rank] ?? ''
+  #suitIcon() {
+    return this.#suitMap()[this.suit].suitImage ?? spade
   }
 
-  attributeChangedCallback(name: string, old: string | null, value: string | null): void {
-    super.attributeChangedCallback(name, old, value)
+  #faceCardImage() {
+    return this.#suitMap()[this.suit][this.rank] ?? ''
+  }
 
-    if (name === 'suit') {
-      this.red = value == 'D' || value == 'H'
+  #suitMap(): { [index: string]: { [index: string]: string } } {
+    return {
+      'C': { suitImage: club, 'J': jc, 'Q': qc, 'K': kc },
+      'D': { suitImage: diamond, 'J': jd, 'Q': qd, 'K': kd },
+      'H': { suitImage: heart, 'J': jh, 'Q': qh, 'K': kh },
+      'S': { suitImage: spade, 'J': js, 'Q': qs, 'K': ks },
     }
   }
 
-  renderCenter() {
+  #renderSide(left = true) {
+    if (this.faceDown) { return null }
+
+    const items = [
+      this.rank,
+      html`<img class='suit' src=${this.#suitIcon()} alt='Suit' />`
+    ]
+
+    return html`
+      <div class='${left ? 'left' : 'right'}'>
+        ${items[left ? 0 : 1]}
+        ${items[left ? 1 : 0]}
+      </div>
+    `
+  }
+
+  #renderCenter() {
+    if (this.faceDown) {
+      return html`<img class='back' src=${back} />`
+    }
+
     return this.rank == 'J' || this.rank == 'Q' || this.rank == 'K'
-      ? html`<img class='face-card ${this.rank}' src=${this.faceCardImage()} />`
-      : html`<img class='suit' src=${this.suitIcon()} alt='Suit' />`;
+      ? html`<img class='face-card ${this.rank}' src=${this.#faceCardImage()} />`
+      : html`<img class='suit' src=${this.#suitIcon()} alt='Suit' />`;
   }
 
   render() {
+    this.classList.toggle('red', this.#isRed())
+
     return html`
-      <div class='left'>
-        ${this.rank}
-        <img class='suit' src=${this.suitIcon()} alt='Suit' />
-      </div>
+      ${this.#renderSide(true)}
       <div class='center'>
-        ${this.renderCenter()}
+        ${this.#renderCenter()}
       </div>
-      <div class='right'>
-        <img class='suit' src=${this.suitIcon()} alt='Suit' />
-        ${this.rank}
-      </div>
+      ${this.#renderSide(false)}
     `
   }
 
   static styles = css`
     :host {
+      --color-black: hsl(348, 8%, 12%);
+      --color-red: hsl(358, 72%, 43%);
+      --current-color: var(--color-black);
+
       --x-aspect: 101.5px;
       --y-aspect: 144px;
-      --scale: 1.5;
+      --scale: 1;
 
-      --color: hsl(348, 8%, 12%);
       --shadow-color: hsl(213, 33%, 71%);
 
       display: grid;
       grid-template-columns: auto 10fr auto;
       background-color: white;
-      color: var(--color);
+      color: var(--current-color);
       box-shadow: 0px 0.125rem 0.25rem 0px var(--shadow-color);
       border-radius: calc(var(--scale) * 0.5rem);
 
@@ -102,21 +116,21 @@ export class OpPlayingCard extends LitElement {
       min-width: calc(var(--scale) * var(--x-aspect));
       height: calc(var(--scale) * var(--y-aspect));
       min-height: calc(var(--scale) * var(--y-aspect));
-      padding: calc(var(--scale) * 0.3125rem);
+      padding: calc(var(--scale) * 0.4rem);
 
       font-size: calc(var(--scale) * 1.25rem);
       box-sizing: border-box;
     }
 
-    :host([red]) {
-      --color: hsl(358, 72%, 43%);
+    :host(.red) {
+      --current-color: var(--color-red);
     }
 
     .suit {
       width: calc(var(--scale) * 0.625rem);
     }
 
-    .face-card {
+    .face-card, .back {
       width: 100%;
     }
 
@@ -140,6 +154,10 @@ export class OpPlayingCard extends LitElement {
 
       &:has(.face-card.J) {
         padding-inline: calc(var(--scale) * 0.3125rem);
+      }
+
+      &:has(.back)  {
+        padding-block: 0;
       }
 
       align-items: center;
